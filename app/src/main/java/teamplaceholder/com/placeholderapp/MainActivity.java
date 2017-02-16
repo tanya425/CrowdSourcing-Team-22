f6484df363689bc2c0503ee24a882e0342192164
RecursiveReincarnation <jswim26@gmail.com>
2017-02-16 13:54:43 -0500

    fixed home screen function
    made it so the app closes when back button is pressed from the main
    screen

--- app/src/main/java/teamplaceholder/com/placeholderapp/MainActivity.java
+++ app/src/main/java/teamplaceholder/com/placeholderapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -19,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onLoginPress(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 }
