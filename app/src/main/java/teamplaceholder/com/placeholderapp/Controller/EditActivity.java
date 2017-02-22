f4a10bcb484ebf75374ccb290189161efba15c99
fishyVishy <vishal.tummalapalli@outlook.com>
2017-02-22 00:47:13 -0500

    finished edit page except for updating database

--- app/src/main/java/teamplaceholder/com/placeholderapp/Controller/EditActivity.java
+++ app/src/main/java/teamplaceholder/com/placeholderapp/Controller/EditActivity.java
@@ -2,14 +2,43 @@ package teamplaceholder.com.placeholderapp.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Arrays;
+import java.util.List;
 
 import teamplaceholder.com.placeholderapp.R;
 
 public class EditActivity extends AppCompatActivity {
+    public static List<String> legalAccountTypes = Arrays.asList("User", "Worker", "Manager", "Admin");
+    private EditText editText;
+    private EditText editText2;
+    private Spinner accountTypeSpinner;
+
+    private String _email;
+    private String _address;
+    private String _accountType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        accountTypeSpinner = (Spinner) findViewById(R.id.spinner2);
+        /*
+          Set up the adapter to display the allowable accounts in the spinner
+         */
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, legalAccountTypes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accountTypeSpinner.setAdapter(adapter);
+    }
+
+    protected void onSubmitPressed(View view) {
+        this.finish();
     }
 }
