ef49e26583e80a9d63b93f3223698d5fd9a7ec68
RecursiveReincarnation <jswim26@gmail.com>
2017-02-28 15:57:06 -0500

    add new icon to app and add water report page

---
+++ app/src/main/java/teamplaceholder/com/placeholderapp/Controller/SubWaterRepoActivity.java
@@ -0,0 +1,14 @@
+package teamplaceholder.com.placeholderapp.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import teamplaceholder.com.placeholderapp.R;
+
+public class SubWaterRepoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_water_repo);
+    }
+}
