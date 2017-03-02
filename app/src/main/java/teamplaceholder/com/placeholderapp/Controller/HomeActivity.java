d372ab756990989e40d1c77a729fc03ed53e4a8c
RecursiveReincarnation <jswim26@gmail.com>
2017-03-02 16:09:18 -0500

    moved home buttons to toolbar

--- app/src/main/java/teamplaceholder/com/placeholderapp/Controller/HomeActivity.java
+++ app/src/main/java/teamplaceholder/com/placeholderapp/Controller/HomeActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import teamplaceholder.com.placeholderapp.R;
@@ -24,6 +25,7 @@ public class HomeActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         loginInfo = getSharedPreferences("login_info", 0);
@@ -35,7 +37,7 @@ public class HomeActivity extends AppCompatActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.home_menu, menu);
         return true;
     }
 
@@ -48,6 +50,7 @@ public class HomeActivity extends AppCompatActivity{
         alert.setTitle("Confirm Logout");
         alert.setMessage("Do you really wish to log out?");
 
+
         alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog,int id) {
                 loginInfoEditor.putString("logged_user", null);
@@ -55,6 +58,7 @@ public class HomeActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
+
         final AlertDialog dialog = alert.create();
         alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog,int id) {
@@ -80,6 +84,25 @@ public class HomeActivity extends AppCompatActivity{
     protected void onAddPress(View view) {
         Intent intent = new Intent(HomeActivity.this, SubWaterRepoActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add_report_toolbar_item) {
+            this.onAddPress(findViewById(R.id.add_report_toolbar_item));
+        } else if (id == R.id.edit_profile_toolbar_item) {
+            this.onEditPress(findViewById(R.id.edit_profile_toolbar_item));
+        } else if (id == R.id.logout_toolbar_item) {
+            this.onLogoutPress(findViewById(R.id.logout_toolbar_item));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
